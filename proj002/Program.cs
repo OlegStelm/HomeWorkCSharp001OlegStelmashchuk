f69dd787a6128e087cb9393f5c43b9bcea9c5c26
Oleg.Stelm <oleg.stelm@gmail.com>
2022-12-02 22:43:09 +0300

    InitCommitAddProj002

---
+++ proj002/Program.cs
@@ -0,0 +1,27 @@
+﻿int a;
+int b;
+int c;
+
+Console.WriteLine("enter a number 'a'");
+int.TryParse(Console.ReadLine(), out a);
+
+Console.WriteLine("enter a number 'b'");
+int.TryParse(Console.ReadLine(), out b);
+
+Console.WriteLine("enter a number 'c'");
+int.TryParse(Console.ReadLine(), out c);
+
+if (a==b && a==c && c==b)
+{
+    Console.Write("The numbers are equal!");
+}
+else
+{
+int max=a;
+if(a>max)max=a;
+if(b>max)max=b;
+if(c>max)max=c;
+
+Console.Write("max=");
+Console.WriteLine(max);
+}

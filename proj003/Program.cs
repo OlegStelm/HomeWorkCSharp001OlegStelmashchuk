4c1382f635fde8006add2bb7b53ef6b15ee8e3bc
Oleg.Stelm <oleg.stelm@gmail.com>
2022-12-02 22:48:49 +0300

    AddProj003

---
+++ proj003/Program.cs
@@ -0,0 +1,13 @@
+﻿int a;
+
+Console.WriteLine("enter a number 'a'");
+int.TryParse(Console.ReadLine(), out a);
+
+if (a%2==0)
+{
+    Console.Write("YES!");
+}
+else
+{
+    Console.Write("NO!");
+}
